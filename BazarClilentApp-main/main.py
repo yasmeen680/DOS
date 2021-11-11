427c4421020d37ae00b3ac353bc58af58e11b2ec
yasmeen680 <71553551+yasmeen680@users.noreply.github.com>
2021-11-11 13:59:19 +0300

    Update main.py

--- BazarClilentApp-main/main.py
+++ BazarClilentApp-main/main.py
@@ -12,7 +12,7 @@ import urllib3
 urllib.request
 import http
 
-d = "\n-----------------------------------------------\n"
+d = "\n-----------------------------------------------\n" 
 i = "info"
 p = "purchase"
 ad = "/admin"
@@ -22,21 +22,21 @@ s = "search"
 cp = "/change_price"
 
 def searchcatagory(topic):
-    req = requests.get('http://172.20.10.5:6060/search/{}'.format(topic))#Return a formatted version of S, using substitutions from args and kwargs
+    req = requests.get('http://10.0.2.14:6060/search/{}'.format(topic))#Return a formatted version of S, using substitutions from args and kwargs
     return req.text
     
 
 def purchaseId(id, name):
-    req = requests.post('http://172.20.10.5:6060/purchase/{}'.format(id), json=({"name": name }))
+    req = requests.post('http://10.0.2.14:6060/purchase/{}'.format(id), json=({"name": name }))
     return req.text
 
 def infoId(id):
-    req = requests.get('http://172.20.10.5:6060/info/{}'.format(id))
+    req = requests.get('http://10.0.2.14:6060/info/{}'.format(id))
     return req.text
    
+    print("\n Welcome  \n")
+    
 commandslist = ["s {title}", "i {item}", "p {item}"]
-
-
 
 Userinput = ""
 while (True) :
@@ -44,7 +44,11 @@ while (True) :
     command = Userinput.split(" ",1) #Return a list of the words in the string
     if Userinput == ex :
         break
-    
+    elif Userinput == help :
+        for x in commandslist :
+            print("  # "+x+"\n")
+    elif len(command) < 2 :
+        print("  invalid command")
     else :
         if command[0] == s:
             print(searchcatagory(command[1]))
@@ -61,4 +65,4 @@ while (True) :
             print("  invalid ")
 
 
-print("Hope You Enjoyed Your Shopping!")
+
